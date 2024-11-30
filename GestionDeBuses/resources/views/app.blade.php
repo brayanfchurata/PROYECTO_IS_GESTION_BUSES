b2ad49b8a15cf33eb63e08d39abffe1f56167a0e
rise29 <74034599@est.unap.edu.pe>
2024-11-30 09:46:45 -0500

    Modificando la interfaz Inicial

--- GestionDeBuses/resources/views/app.blade.php
+++ GestionDeBuses/resources/views/app.blade.php
@@ -14,6 +14,9 @@
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
+
+        <!-- Vincula el archivo CSS -->
+        <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     </head>
     <body class="font-sans antialiased">
         @inertia
